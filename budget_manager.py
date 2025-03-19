6a0757f005eb59de220313736b766f441acdd28e
desmondcoacher <desmond.c@campus.technion.ac.il>
2025-03-19 08:58:43 +0200

    Added menu handler as a defined function

--- budget_manager.py
+++ budget_manager.py
@@ -6,41 +6,43 @@
 #  ]
 # }
 
-while True:
+import transactions  # Importing functions module
 
-    print("\n" + "*" * 25)
-    print("Welcome to Budget Manager")
-    print("*" * 25 + "\n")
+budget_data = {}    # Creating new empty dictionary for all transactions
 
-    print("1. Add Income")
-    print("2. Add Expense")
-    print("3. Show Balance")
-    print("4. Show Transaction History")
-    print("5. Exit\n")
+while True: # Loop Menu Start
+
+    transactions.menu_handler() # Calling defined function from transactions imported module (Menu Handler)
     
-    userChoise = input("Enter menu option: ")
+    userChoise = input("\nEnter menu option: ") # # Asking the user to make a choise
 
     match userChoise:
 
-        case "1":
+        case "1":   # Add Income
             print("Menu option 1 has been choosen.")
-            pass
+            # pass
+            transactions.add_income()   # Calling defined function from transactions imported module
 
-        case "2":
+        case "2":   # Add Expense
             print("Menu option 2 has been choosen.")
-            pass
+            # pass
+            transactions.add_expense()   # Calling defined function from transactions imported module
 
-        case "3":
+        case "3":   # Show Balance
             print("Menu option 3 has been choosen.")
-            pass
+            # pass
+            transactions.show_balance()   # Calling defined function from transactions imported module
 
-        case "4":
+        case "4":   # Show Transaction History
             print("Menu option 4 has been choosen.")
-            pass
+            # pass
+            transactions.show_transaction_history()   # Calling defined function from transactions imported module
 
-        case "5":
-            print("Menu option 5 has been choosen.")
-            break
+        case "5":   # Exit
+            print("Thank you for using our service. See you later!\n")   # Print goodbye message before quit
+            break # Breaking the loop (quit)
 
-        case _:
-            print("Error: Incorrect menu option has been choosen. Please try again.\n")
+        case _: # In case that the user's choosen option is not exists
+            print("Error: Incorrect menu option has been choosen. Please try again.")   # Print error message in case that incorrect menu option has choosen
+    input("Press Enter to return to the Main Menu.")    # Prompt the input message each time into the loop for returning to Main Menu
+# Loop Menu End
