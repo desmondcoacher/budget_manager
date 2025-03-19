b6eb3063f6088715b2638fc5ca8749134bd4125c
desmondcoacher <desmond.c@campus.technion.ac.il>
2025-03-19 08:58:00 +0200

    Built menu skeleton in main file and functions skeleton in transactions  file

---
+++ transactions.py
@@ -0,0 +1,40 @@
+# This is a transactions functions file which will be used in main file
+
+# budget_data = {
+#  "balance": 500,
+#  "transactions": [
+#  {"type": "income", "amount": 1000, "description": "Salary"},
+#  {"type": "expense", "amount": 500, "description": "Groceries"}
+#  ]
+# }
+
+# import transactions  # Importing functions module
+
+budget_data = {}    # Creating new empty dictionary for all transactions
+
+def menu_handler(): # Main Menu Handler
+    print("\n" + "*" * 25)
+    print("Welcome to Budget Manager")
+    print("*" * 25 + "\n")
+
+    print("1. Add Income")
+    print("2. Add Expense")
+    print("3. Show Balance")
+    print("4. Show Transaction History")
+    print("5. Exit")
+
+def add_income():   # Add Income Menu Option
+    print("Add Income option has been choosen.")    # For test purposes
+    pass    # For test purposes
+
+def add_expense():  # Add Expense Menu Option
+    print("Add Expense option has been choosen.")    # For test purposes
+    pass    # For test purposes
+
+def show_balance(): # Show Balance Menu Option
+    print("Show Balance option has been choosen.")    # For test purposes
+    pass    # For test purposes
+
+def show_transaction_history(): # Show Transaction History Menu Option
+    print("Show Transaction History option has been choosen.")    # For test purposes
+    pass    # For test purposes
